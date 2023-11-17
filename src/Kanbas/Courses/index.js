3e1bf6ce524aa0f8638d01b1e4a83be634a5dda9
DESKTOP-U8GBATB\ahmad <ahmadelidrissiamiri@gmail.com>
2023-11-17 09:57:08 -0500

    fix6

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -16,7 +16,10 @@ import "./courses.css";
 function Courses() {
   const { courseId } = useParams();
   const [course, setCourse] = useState({});
-  const URL = "http://localhost:4000/api/courses";
+  //const URL = "http://localhost:4000/api/courses";
+  const URL = `${API_BASE}/courses`;
+  const API_BASE = process.env.REACT_APP_API_BASE;
+
   const findCourseById = async (courseId) => {
     const response = await axios.get(
       `${URL}/${courseId}`
